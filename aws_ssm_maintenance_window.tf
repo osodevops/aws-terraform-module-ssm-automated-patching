3bee0ae98afe62fce0681d466f284f73168e0096
Sion Smith <sion.smith@gmail.com>
2019-10-24 12:14:18 +0900

    - updated cron to run 07:00 every Sunday - Corrected baseline filters - Changes from scan to install

--- aws_ssm_maintenance_window.tf
+++ aws_ssm_maintenance_window.tf
@@ -2,8 +2,7 @@ resource "aws_ssm_maintenance_window" "patching_window" {
   name              = "patching-window-${var.patching_name}"
   enabled           = true
   schedule_timezone = var.schedule_timezone
-//  schedule          = "cron(* 21 * * ? *)"
-  schedule          = "cron(0/30 * * * ? *)"
-  duration          = 1
+  schedule          = "cron(0 0 7 ? * SUN *)"
+  duration          = 5
   cutoff            = 0
 }
--- aws_ssm_patch_baseline.tf
+++ aws_ssm_patch_baseline.tf
@@ -3,16 +3,21 @@ resource "aws_ssm_patch_baseline" "patch_security_baseline" {
   name                              = "patching-security-baseline-${var.patching_name}"
   description                       = "Patch baseline for ${var.application_tag_name}"
   operating_system                  = var.operating_system_filter
-  approved_patches_compliance_level = "CRITICAL"
 
   approval_rule {
-    approve_after_days  = 0
-    compliance_level    = "CRITICAL"
-    enable_non_security = false
-
+    approve_after_days = 0
+    compliance_level   = "CRITICAL"
     patch_filter {
-      key    = "CLASSIFICATION"
-      values = ["Security"]
+      key    = "SEVERITY"
+      values = ["Critical"]
+    }
+  }
+  approval_rule {
+    approve_after_days = 7
+    compliance_level   = "HIGH"
+    patch_filter {
+      key    = "SEVERITY"
+      values = ["Low", "Important", "Moderate"]
     }
   }
 }
